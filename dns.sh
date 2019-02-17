4c2fbd76cbc594cef0966047ff1ce4072094f49c
Druchem <mrdruchem@gmail.com>
2019-02-18 00:25:31 +0100

    Fixed typo in DNS script and added comments.

--- dns.sh
+++ dns.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
+CONFIGURATION_FILE=/etc/bind/named.conf.options
+
 # Install bind9 DNS server
 apt-get -y install bind9
 
-echo -e "acl clients {" >> /etc/bind/named.conf.options
-echo -e "\tlocalhost;" >> /etc/bind/named.conf.options
-echo -e "\t$LAN_SUBNET_CIDR;" >> /etc/bind/named.conf.options
-echo -e "};" >> /etc/bind/named.conf.options
-echo -e "options {" >> /etc/bind/named.conf.options
-echo -e "\tforwarders {" >> /etc/bind/named.conf.options
-echo -e "\t\t$DNS_UPSTREAM_1;" >> /etc/bind/named.conf.options
-echo -e "\t\t$DNS_UPSTREAM_2;" >> /etc/bind/named.conf.options
-echo -e "\t};" >> /etc/bind/named.conf.options
-echo -e "\trecursion yes;" >> /etc/bind/named.conf.options
-echo -e "\allow-query { clients; };" >> /etc/bind/named.conf.options
-echo -e "\tforward only;" >> /etc/bind/named.conf.options
-echo -e "};" >> /etc/bind/named.conf.options
+# Remove old configuration file
+mv $CONFIGURATION_FILE $CONFIGURATION_FILE.old
 
+# Write new configuration file
+echo -e "acl clients {" >> $CONFIGURATION_FILE
+echo -e "\tlocalhost;" >> $CONFIGURATION_FILE
+echo -e "\t$LAN_SUBNET_CIDR;" >> $CONFIGURATION_FILE
+echo -e "};" >> $CONFIGURATION_FILE
+echo -e "options {" >> $CONFIGURATION_FILE
+echo -e "\tdirectory \"/var/cache/bind\";" >> $CONFIGURATION_FILE
+echo -e "\tforwarders {" >> $CONFIGURATION_FILE
+echo -e "\t\t$DNS_UPSTREAM_1;" >> $CONFIGURATION_FILE
+echo -e "\t\t$DNS_UPSTREAM_2;" >> $CONFIGURATION_FILE
+echo -e "\t};" >> $CONFIGURATION_FILE
+echo -e "\trecursion yes;" >> $CONFIGURATION_FILE
+echo -e "\tallow-query { clients; };" >> $CONFIGURATION_FILE
+echo -e "\tforward only;" >> $CONFIGURATION_FILE
+echo -e "};" >> $CONFIGURATION_FILE
+
+# Restart DNS server to apply changes
 service restart bind9
